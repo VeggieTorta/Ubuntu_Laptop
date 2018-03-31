ca8507f3f14ae8042aa0beb51660cb23471f5d3a
VeggieTorta <30427052+VeggieTorta@users.noreply.github.com>
2018-03-30 21:20:43 -0700

    Update .Init.Intall.sh

--- .Init.Intall.sh
+++ .Init.Intall.sh
@@ -1,3 +1,3 @@
-# Concept: Install or remove multiple programs, making a fresh install of an OS less of a pain. 
+# Concept: Install or remove multiple programs, making a fresh install of an OS less of a pain. Using README.md as guide.
 
 sudo apt-get install package1 package2 package3 -y
