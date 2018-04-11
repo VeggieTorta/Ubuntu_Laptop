d323d5b348fe882a967715ebdf0e1821f83e446f
VeggieTorta <ochoaje@me.com>
2018-04-11 13:33:15 -0700

    update added pia info

--- Initial.Installer.sh
+++ Initial.Installer.sh
@@ -4,9 +4,18 @@ sudo apt update
 sudo apt-get install -y gdebi
 
 
-###################
-## google chrome ##
-###################
+############################
+##          scripts       ##
+############################
+
+# NEEDS WORK
+# sudo mv current/file /home/vt/
+
+
+
+############################
+##     google chrome      ##
+############################
 
 # dependency pkgs
 sudo apt-get install -y libxss1 libappindicator1 libindicator7
@@ -15,11 +24,32 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 # install google-chrome pkg
 sudo dpkg -i google-chrome*.deb
 
-##################
-##      git     ##
-##################
+
+
+###########################
+##           git         ##
+###########################
 
 sudo apt install -y git
+
+
+
+###########################
+##         PIA           ##
+###########################
+
+    # ubuntu openvpn
+#sudo apt-get install -y curl network-manager-openvpn-gnome
+#wget https://www.privateinternetaccess.com/installer/pia-nm.sh
+#sudo bash pia-nm.sh
+
+# desktop app (NEEDS WORK)
+# cd Downloads
+# wget https://www.privateinternetaccess.com/installer/download_installer_linux
+# tar -xzf pia-v78-installer-linux.tar.gz
+# ./pia-v78-installer-linux.sh
+
+
 
 ##############################
 ##     tmux & dependencies  ##
@@ -40,24 +70,30 @@ sudo make install -y
 cd -
 rm -fr /tmp/tmux
 
-#####################
-##     gitKraken   ##
-#####################
+
+
+############################
+##        gitKraken       ##
+############################
 
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i --force-depends gitkraken-amd64.deb
 sudo apt-get install -f
 rm -f gitkraken-amd64.deb
 
-#############
-## ranger  ##
-#############
+
+
+############################
+##         ranger         ##
+############################
 
 sudo apt-get install -y ranger caca-utils highlight atool w3m poppler-utils mediainfo
 
-#############
-##   vim   ##
-#############
+
+
+############################
+##          vim           ##
+############################
 
 yes sudo '' | add-apt-repository ppa:jonathonf/vim
 sudo apt install -y vim
@@ -68,9 +104,10 @@ sudo apt install -y vim
 # vim vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-##########################
-##     gsettings        ##
-##########################
+
+###########################
+##     gsettings         ##
+###########################
 
 # Unit bar move to bottom of screen
 gsettings set com.canonical.Unity.Launcher launcher-position Bottom
@@ -83,15 +120,16 @@ gsettings set org.gnome.desktop.background picture-uri 'file:///home/vt/Pictures
 
 
 
-########################
-##  gnome-tweak-tool  ##
-########################
+############################
+##    gnome-tweak-tool    ##
+############################
 
 sudo apt-get install -y gnome-tweak-tool
 
-####################
-# Tilix terminal  ##
-####################
+
+############################
+##     Tilix terminal     ##
+############################
 
 # dl and place in /tmp/tilix
 wget https://github.com/gnunn1/tilix/releases/download/1.7.7/tilix.zip /tmp/tilix
@@ -104,16 +142,16 @@ sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
 # Remove tmp/tilix folders & files
 rm -fr /tmp/tilix
 
-###############
-##    cmus   ##
-###############
+############################
+##          cmus          ##
+############################
 
 yes '' | sudo add-apt-repository ppa:jmuc/cmus
 sudo apt-get install -y cmus
 
-###########################
-##    cava (NOT WORKING) ##
-###########################
+############################
+##    cava (NOT WORKING)  ##
+############################
 
 apt-get install -y libfftw3-dev libasound2-dev libncursesw5-dev libpulse-dev libtool
 
@@ -121,15 +159,15 @@ yes sudo '' | add-apt-repository ppa:tehtotalpwnage/ppa
 sudo apt-get update
 sudo apt-get install -y cava
 
-##################
-##     mutt     ##
-##################
+############################
+##          mutt          ##
+############################
 
 sudo apt-get install -y mutt
 
-###################
-##  paper theme  ##
-###################
+############################
+##       paper theme      ##
+############################
 
 yes '' | sudo add-apt-repository ppa:snwh/pulp
 sudo apt-get update
@@ -143,15 +181,15 @@ sudo apt-get install -y paper-cursor-theme
 
 sudo apt-get install -y unity-tweak-tool
 
-######################
-## remove bloatwear ##
-######################
+###########################
+##    remove bloatwear   ##
+###########################
 
 sudo apt-get remove account-plugin-aim account-plugin-facebook account-plugin-flickr account-plugin-jabber account-plugin-salut account-plugin-twitter account-plugin-windows-live account-plugin-yahoo aisleriot brltty colord deja-dup deja-dup-backend-gvfs duplicity empathy empathy-common evolution-data-server-online-accounts example-content firefox gnome-accessibility-themes gnome-contacts gnome-mahjongg gnome-mines gnome-orca gnome-screensaver gnome-sudoku gnome-video-effects gnomine landscape-common libreoffice-avmedia-backend-gstreamer libreoffice-base-core libreoffice-calc libreoffice-common libreoffice-core libreoffice-draw libreoffice-gnome libreoffice-gtk libreoffice-impress libreoffice-math libreoffice-ogltrans libreoffice-pdfimport libreoffice-presentation-minimizer libreoffice-style-galaxy libreoffice-style-human libreoffice-writer libsane libsane-common mcp-account-manager-uoa python3-uno rhythmbox rhythmbox-plugins rhythmbox-plugin-zeitgeist sane-utils shotwell shotwell-common telepathy-gabble telepathy-haze telepathy-idle telepathy-indicator telepathy-logger telepathy-mission-control-5 telepathy-salut thunderbird thunderbird-gnome-support totem totem-common totem-plugins unity-scope-audacious unity-scope-chromiumbookmarks unity-scope-clementine unity-scope-colourlovers unity-scope-devhelp unity-scope-firefoxbookmarks unity-scope-gdrive unity-scope-gmusicbrowser unity-scope-gourmet unity-scope-manpages unity-scope-musicstores unity-scope-musique unity-scope-openclipart unity-scope-texdoc unity-scope-tomboy unity-scope-video-remote unity-scope-virtualbox unity-scope-yelp unity-scope-zotero -y
 
-#######################
-## Power Saving Tool ##
-#######################
+##########################
+##  Power Saving Tool   ##
+##########################
   
   #powertop
 sudo apt-get update
